d1a13cc24b18d3181528507cb262386c6669bd52
Anar-Qandayev <tu7bxfga8@code.edu.az>
2022-03-08 23:24:45 +0400

    commit

---
+++ num.length/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace num.length
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Eded daxil edin!.");
+
+            int num = Convert.ToInt32(Console.ReadLine());
+            int count = NumLength(num);
+            Console.WriteLine(count);
+        }
+         static int NumLength(int num)
+        {
+            int count = 0;
+            while (num > 0)
+            {
+                num = num / 10;
+                count++;
+            }
+            return count;
+
+        }
+    }
+}
